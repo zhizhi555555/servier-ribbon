fbeefbef7901f21a5e33928b9e8d3bb7c589c4a7
zhaoyang <zhaoyang@ixiaopu.com>
2018-04-26 11:40:31 +0800

    Initial commit

---
+++ src/test/java/com/example/servierribbon/ServierRibbonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.servierribbon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServierRibbonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
